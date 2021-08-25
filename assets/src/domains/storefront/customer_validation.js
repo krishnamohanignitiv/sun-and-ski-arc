50f7b2750b05964b91ce395351a90a6c70f6deab
rkDas2949 <ranjeet.d@ignitiv.com>
2021-08-25 18:53:12 +0530

    customer_validation code is deployed

--- assets/src/domains/storefront/customer_validation.js
+++ assets/src/domains/storefront/customer_validation.js
@@ -21,6 +21,80 @@
 
  */
 
-module.exports = function(context, callback) {
-  callback();
+const request = require('requests');
+
+module.exports = function (context, callback) {
+
+  request('https://api.simpleapps.net/ecommerce/customers/100004?resource_list=all', {
+    method: 'GET',
+    headers: {
+      'x-api-key': '020A1B0AD2E19A2C13931F6744BC52C096FF5BB0',
+      'siteid': 'coastalone'
+    }
+
+  }).on('data', function (customervalidation) {
+    console.log("Customer Data", customervalidation);
+    context.response.body = customervalidation;
+    context.response.end();
+
+    request('https://api.simpleapps.net/ecommerce/invoices?resource_list=all&customer_id=100004&limit=5', {
+        method: 'GET',
+        headers: {
+          'x-api-key': '020A1B0AD2E19A2C13931F6744BC52C096FF5BB0',
+          'siteid': 'coastalone'
+        }
+
+      }).on('data', function (invoicevalidation) {
+        console.log("Invoice Data", invoicevalidation);
+        context.response.body = invoicevalidation;
+        context.response.end();
+      })
+      .on('end', function (err) {
+        if (err) return console.log('connection closed due to errors', err);
+
+        console.log('end');
+        callback();
+      });
+
+    
+  }).on('end', function (err) {
+    if (err) return console.log('connection closed due to errors', err);
+
+    console.log('end');
+    callback();
+  });
+
+
+  console.info('Initiating simpleappsapi and added customer data request');
+
+
+
+  // //Invoice
+
+  // request('https://api.simpleapps.net/ecommerce/invoices?resource_list=all&customer_id=100004&limit=5', {
+  //     method: 'GET',
+  //     headers: {
+  //       'x-api-key': '020A1B0AD2E19A2C13931F6744BC52C096FF5BB0',
+  //       'siteid': 'coastalone'
+  //     }
+
+  //   })
+
+  //   .on('data', function (chunk) {
+  //     console.log("Invoice Data", chunk);
+  //     context.response.body = chunk;
+  //     context.response.end();
+  //   })
+  //   .on('end', function (err) {
+  //     if (err) return console.log('connection closed due to errors', err);
+
+  //     console.log('end');
+  //     callback();
+  //   });
+
+
+  // console.info('Initiating simpleappsapi and added Invoice data request');
+
+
+  //callback();
 };
