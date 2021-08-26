ed5cf43b2caf8cb1caddf0e019ae4ead7038f1aa
Nawanit <nawanit.s@ignitiv.com>
2021-08-26 12:52:04 +0545

    fixed eslint warnings except customer_validation.js

--- assets/src/domains/storefront/b2b_user_creation.js
+++ assets/src/domains/storefront/b2b_user_creation.js
@@ -1,31 +1,9 @@
 const B2bAccountSDK = require('mozu-node-sdk/clients/commerce/customer/b2BAccount');
 const Client = require('mozu-node-sdk/clients/platform/application');
-/**
- * Implementation for http.storefront.routes
 
-
- * HTTP Actions all receive a similar context object that includes
- * `request` and `response` objects. These objects are similar to
- * http.IncomingMessage objects in NodeJS.
-
-{
-  configuration: {},
-  request: http.ClientRequest,
-  response: http.ClientResponse
-}
-
- * Call `response.end()` to end the response early.
- * Call `response.set(headerName)` to set an HTTP header for the response.
- * `request.headers` is an object containing the HTTP headers for the request.
- * 
- * The `request` and `response` objects are both Streams and you can read
- * data out of them the way that you would in Node.
-
- */
-
-module.exports = function(context) {
+module.exports = function (context) {
   const payload = Object.assign({}, context.request.body); // eslint-disable-line prefer-object-spread
-  const client = new Client({ // eslint-disable-line global-require
+  const client = new Client({
     context: {
       tenant: 30374,
       site: 50935,
--- assets/src/storefront.manifest.js
+++ assets/src/storefront.manifest.js
@@ -1,11 +1,11 @@
+/* eslint-disable global-require */
 module.exports = {
-  
-  'customer_validation': {
-      actionName: 'http.storefront.routes',
-      customFunction: require('./domains/storefront/customer_validation')
+  customer_validation: {
+    actionName: 'http.storefront.routes',
+    customFunction: require('./domains/storefront/customer_validation'),
   },
-  'b2b_user_creation': {
-      actionName: 'http.storefront.routes',
-      customFunction: require('./domains/storefront/b2b_user_creation')
-  }
+  b2b_user_creation: {
+    actionName: 'http.storefront.routes',
+    customFunction: require('./domains/storefront/b2b_user_creation'),
+  },
 };
