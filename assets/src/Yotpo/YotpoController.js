019cfe93e43436e39adebb891c07e44dd26b984c
Nawanit <nawanit.s@ignitiv.com>
2021-10-12 17:36:30 +0545

    Able to get response containing yotpo's order's yotpo_id for createOrder

--- assets/src/Yotpo/YotpoController.js
+++ assets/src/Yotpo/YotpoController.js
@@ -3,12 +3,26 @@ const superagent = require('superagent');
 const Client = require('mozu-node-sdk/clients/platform/application');
 
 class YotpoController {
-  async createOrder(payload) {
-    const response = await superagent
-      .post('https://api.yotpo.com/core/v3/stores/nCDyz6kmkjSpnUisjmeG3kunobKNEDGTN1fB8xlR/orders')
+  constructor() {
+    this.yotpoOrderId = '';
+  }
+
+  createOrder(payload) {
+    return superagent
+      .post(
+        'https://api.yotpo.com/core/v3/stores/nCDyz6kmkjSpnUisjmeG3kunobKNEDGTN1fB8xlR/orders'
+      )
       .set('X-Yotpo-Token', 'RPFUpHShGSfUfRQNiTmVyacIJB7brk4penL0cmWK')
-      .send(payload);
-    return response;
+      .set('Content-Type', 'application/json')
+      .send(payload)
+      .then(res => {
+        this.yotpoOrderId = res.body.order.yotpo_id;
+        console.log(this.yotpoOrderId);
+        return res.body;
+      })
+      .catch(err => {
+        throw new Error(err.response.body.errors[0].message);
+      });
   }
 }
 
--- assets/src/domains/commerce.orders/embedded.commerce.orders.action.after.js
+++ assets/src/domains/commerce.orders/embedded.commerce.orders.action.after.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const Client = require('mozu-node-sdk/clients/platform/application');
 const PriceListController = require('../../pricelist/PriceListController');
 const EntityListController = require('../../EntityList/EntityListController');
@@ -9,10 +10,12 @@ const yotpoOrder = new YotpoController();
 
 module.exports = function (context, callback) {
   const order = context.get.order();
+
   if (order.status === 'Accepted') {
     const quoteExtendedProperty = order.extendedProperties.find(data => data.key === 'quoteId');
 
     console.log(quoteExtendedProperty);
+    console.log(order.billingInfo.billingContact);
 
     /**
      * Yotpo
@@ -21,23 +24,33 @@ module.exports = function (context, callback) {
       quantity: product.quantity,
       external_product_id: product.product.productCode
     }));
+
     // eslint-disable-next-line no-unused-vars
     const yotpoPayload = {
       order: {
-        external_id: order.id,
+        external_id: order.orderNumber,
         order_date: order.acceptedDate,
+        currencyCode: order.currencyCode,
         customer: {
           external_id: order.billingInfo.billingContact.id,
           email: order.billingInfo.billingContact.email,
           first_name: order.billingInfo.billingContact.firstName,
-          last_name: order.billingInfo.billingContact.lastName,
+          last_name: order.billingInfo.billingContact.lastNameOrSurname,
           accepts_sms_marketing: false,
           accepts_email_marketing: false
         },
         line_items: yotpoLineItems,
-        fulfillments: []
+        fulfillments: [{
+          fulfillment_date: order.acceptedDate,
+          external_id: order.orderNumber,
+          status: 'pending',
+          fulfilled_items: yotpoLineItems
+        }]
       }
     };
+
+    console.log(yotpoPayload.fulfillments);
+    console.log(yotpoLineItems);
     // Yotpo End
 
     if (!quoteExtendedProperty) {
@@ -45,6 +58,37 @@ module.exports = function (context, callback) {
       yotpoOrder.createOrder(yotpoPayload)
         .then(res => {
           console.log(res);
+          console.log('Order Created');
+          callback();
+        })
+        .catch(err => {
+          console.log(err);
+          callback();
+        });
+    } else {
+      const productToUpdate = order.items.map(product => ({
+        productCode: product.product.productCode,
+        availableQty: product.quantity
+      }));
+
+      const payload = {
+        entityListFQN: 'quotelist@coastal',
+        quoteId: quoteExtendedProperty.value,
+        productToUpdate
+      };
+
+      const clientContext = new Client();
+
+      const promises = [entityList.updateEntity(clientContext, payload), priceList.updateB2BAccount(context, {
+        accountId: order.customerAccountId,
+        priceListCode: 'default'
+      })];
+
+      Promise.all(promises)
+        .then(() => yotpoOrder.createOrder(yotpoPayload))
+      // eslint-disable-next-line no-unused-vars
+        .then(res => {
+          console.log('Done');
           callback();
         })
         .catch(err => {
@@ -52,50 +96,8 @@ module.exports = function (context, callback) {
           callback();
         });
     }
-
-    const productToUpdate = order.items.map(product => ({
-      productCode: product.product.productCode,
-      availableQty: product.quantity
-    }));
-
-    const payload = {
-      entityListFQN: 'quotelist@coastal',
-      quoteId: quoteExtendedProperty.value,
-      productToUpdate
-    };
-
-    const clientContext = new Client();
-
-    const promises = [entityList.updateEntity(clientContext, payload), priceList.updateB2BAccount(context, {
-      accountId: order.customerAccountId,
-      priceListCode: 'default'
-    })];
-
-    Promise.all(promises)
-      .then(() => yotpoOrder.createOrder(yotpoPayload))
-    // eslint-disable-next-line no-unused-vars
-      .then(res => {
-        console.log('Done');
-        callback();
-      })
-      .catch(err => {
-        console.log(err);
-        callback();
-      });
-
-    // entityList
-    //   .updateEntity(clientContext, payload)
-    // // eslint-disable-next-line no-unused-vars
-    //   .then(res => {
-    //   // console.log(res);
-    //     callback();
-    //   });
-
-    // priceList
-    // .updateB2BAccount(clientContext,{
-
-    // })
   } else {
+    console.log('Unwanted Console');
     callback();
   }
 };
