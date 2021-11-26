5d7c7bc03f5e5b357600edfe6212d9d5d6335750
Nawanit <nawanit.s@ignitiv.com>
2021-11-26 12:42:26 +0530

    Removed Yotpo Order creating from order.status= Accepted action

--- assets/src/domains/commerce.orders/embedded.commerce.orders.action.after.js
+++ assets/src/domains/commerce.orders/embedded.commerce.orders.action.after.js
@@ -17,54 +17,54 @@ module.exports = function (context, callback) {
     console.log(quoteExtendedProperty);
     console.log(order.billingInfo.billingContact);
 
-    /**
-     * Yotpo
-     */
-    const yotpoLineItems = order.items.map(product => ({
-      quantity: product.quantity,
-      external_product_id: product.product.productCode
-    }));
+    // /**
+    //  * Yotpo
+    //  */
+    // const yotpoLineItems = order.items.map(product => ({
+    //   quantity: product.quantity,
+    //   external_product_id: product.product.productCode
+    // }));
 
-    // eslint-disable-next-line no-unused-vars
-    const yotpoPayload = {
-      order: {
-        external_id: order.orderNumber,
-        order_date: order.acceptedDate,
-        currencyCode: order.currencyCode,
-        customer: {
-          external_id: order.billingInfo.billingContact.id,
-          email: order.billingInfo.billingContact.email,
-          first_name: order.billingInfo.billingContact.firstName,
-          last_name: order.billingInfo.billingContact.lastNameOrSurname,
-          accepts_sms_marketing: false,
-          accepts_email_marketing: false
-        },
-        line_items: yotpoLineItems,
-        fulfillments: [{
-          fulfillment_date: order.acceptedDate,
-          external_id: order.orderNumber,
-          status: 'pending',
-          fulfilled_items: yotpoLineItems
-        }]
-      }
-    };
+    // // eslint-disable-next-line no-unused-vars
+    // const yotpoPayload = {
+    //   order: {
+    //     external_id: order.orderNumber,
+    //     order_date: order.acceptedDate,
+    //     currencyCode: order.currencyCode,
+    //     customer: {
+    //       external_id: order.billingInfo.billingContact.id,
+    //       email: order.billingInfo.billingContact.email,
+    //       first_name: order.billingInfo.billingContact.firstName,
+    //       last_name: order.billingInfo.billingContact.lastNameOrSurname,
+    //       accepts_sms_marketing: false,
+    //       accepts_email_marketing: false
+    //     },
+    //     line_items: yotpoLineItems,
+    //     fulfillments: [{
+    //       fulfillment_date: order.acceptedDate,
+    //       external_id: order.orderNumber,
+    //       status: 'pending',
+    //       fulfilled_items: yotpoLineItems
+    //     }]
+    //   }
+    // };
 
-    console.log(yotpoPayload.fulfillments);
-    console.log(yotpoLineItems);
-    // Yotpo End
+    // console.log(yotpoPayload.fulfillments);
+    // console.log(yotpoLineItems);
+    // // Yotpo End
 
     if (!quoteExtendedProperty) {
       console.log('No quote extendedProperty');
-      yotpoOrder.createOrder(yotpoPayload)
-        .then(res => {
-          console.log(res);
-          console.log('Order Created');
-          callback();
-        })
-        .catch(err => {
-          console.log(err);
-          callback();
-        });
+      // yotpoOrder.createOrder(yotpoPayload)
+      //   .then(res => {
+      //     console.log(res);
+      //     console.log('Order Created');
+      //     callback();
+      //   })
+      //   .catch(err => {
+      //     console.log(err);
+      //     callback();
+      //   });
     } else {
       const productToUpdate = order.items.map(product => ({
         productCode: product.product.productCode,
@@ -85,7 +85,7 @@ module.exports = function (context, callback) {
       })];
 
       Promise.all(promises)
-        .then(() => yotpoOrder.createOrder(yotpoPayload))
+        // .then(() => yotpoOrder.createOrder(yotpoPayload))
       // eslint-disable-next-line no-unused-vars
         .then(res => {
           console.log('Done');
