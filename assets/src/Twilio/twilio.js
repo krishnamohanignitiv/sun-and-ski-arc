09587d60e9fa3d3656c84f566bafee4afa05620c
Nawanit <nawanit.s@ignitiv.com>
2021-12-14 20:50:45 +0530

    Twilio controller written and connected with sendOtp and verifyOTP storefront route.

--- assets/src/Twilio/twilio.js
+++ assets/src/Twilio/twilio.js
@@ -1,14 +1,21 @@
-// const accountSid = 'AC260b9f814e747c9f528d70164e27dd9b';
-// const authToken = '1d35d9c0aa7c89cf7d28a0c43f87305e';
-// const client = require('twilio')(accountSid, authToken);
+const accountSid = 'AC260b9f814e747c9f528d70164e27dd9b';
+const authToken = '1d35d9c0aa7c89cf7d28a0c43f87305e';
+const client = require('twilio')(accountSid, authToken);
 
 class TwilioController {
   // eslint-disable-next-line no-unused-vars
-  smsOTPsend(phnNumber) {
-    console.log('Hello World');
-    // return client.verify.services('VAaf49f2af1ff3f84cebeed1eb11a6b15f')
-    //   .verifications
-    //   .create({ to: phnNumber, channel: 'sms' });
+  async smsOTPsend(phnNumber) {
+    return client.verify.services('VAaf49f2af1ff3f84cebeed1eb11a6b15f')
+      .verifications
+      .create({ to: phnNumber, channel: 'sms' });
+    // .then(verification => console.log(verification))
+    // .catch(err => console.log(err));
+  }
+
+  async otpVerify(phnNumber, enteredOTP) {
+    return client.verify.services('VAaf49f2af1ff3f84cebeed1eb11a6b15f')
+      .verificationChecks
+      .create({ to: phnNumber, code: enteredOTP });
     // .then(verification => console.log(verification))
     // .catch(err => console.log(err));
   }
--- assets/src/domains/storefront/twilio_otp_verify.js
+++ assets/src/domains/storefront/twilio_otp_verify.js
@@ -3,20 +3,18 @@ const Twilio = require('../../Twilio/twilio');
 const twilio = new Twilio();
 
 module.exports = function (context) {
-  const { phnNumber } = context.request.body;
-  twilio.smsOTPsend(phnNumber)
+  const { phnNumber, enteredOTP } = context.request.body;
+
+  twilio.otpVerify(phnNumber, enteredOTP)
     .then(verification => {
-      console.log(verification);
+      if (verification.valid === false) throw new Error('Value did not match!!');
       context.response.body = 'success';
       context.response.end();
     })
     .catch(err => {
       console.log(err);
-      context.response.status = err.status || 400;
-      context.response.body = err;
+      context.response.status = 400;
+      context.response.body = err.message || err;
       context.response.end();
     });
-  // console.log('Hello World');
-  // context.response.body = 'Hello World';
-  // context.response.end();
 };
