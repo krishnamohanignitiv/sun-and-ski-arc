e4bd10679c141b34efa1d36dfd7c7c02e6780a5a
Nawanit <nawanit.s@ignitiv.com>
2021-12-30 16:31:31 +0530

    Revised response payload for twilio_otpSend function.

--- assets/src/domains/storefront/twilio_otp_send.js
+++ assets/src/domains/storefront/twilio_otp_send.js
@@ -7,13 +7,22 @@ module.exports = function (context) {
   twilio.smsOTPsend(phnNumber)
     .then(verification => {
       console.log(verification);
-      context.response.body = 'success';
+      const responseBody = {
+        success: true,
+        attempts: verification.sendCodeAttempts.length,
+      };
+      context.response.body = responseBody;
       context.response.end();
     })
     .catch(err => {
       console.log(err);
       context.response.status = err.status || 400;
-      context.response.body = err;
+      const responseBody = {
+        success: false,
+        message: err.message,
+        status: err.status
+      };
+      context.response.body = responseBody;
       context.response.end();
     });
   // console.log('Hello World');
