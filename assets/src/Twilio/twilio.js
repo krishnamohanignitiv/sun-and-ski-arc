1d5380c1c4eaf35a10b27ac3f0daa217f200306a
Nawanit <nawanit.s@ignitiv.com>
2022-01-07 11:35:44 +0530

    Updated Twilio Account Details| Added functions.json to gitignore

--- assets/src/Twilio/twilio.js
+++ assets/src/Twilio/twilio.js
@@ -1,11 +1,13 @@
-const accountSid = 'AC260b9f814e747c9f528d70164e27dd9b';
-const authToken = '1d35d9c0aa7c89cf7d28a0c43f87305e';
+const accountSid = 'AC826ca5988ab01691b65052d2ced72f60';
+const authToken = '4266d7d489e38e4220888c1e3a6ee7d6';
 const client = require('twilio')(accountSid, authToken);
+
+const serviceId = 'VA8232d8575376a9273b94784f60f6b962';
 
 class TwilioController {
   // eslint-disable-next-line no-unused-vars
   async smsOTPsend(phnNumber) {
-    return client.verify.services('VAaf49f2af1ff3f84cebeed1eb11a6b15f')
+    return client.verify.services(serviceId)
       .verifications
       .create({ to: phnNumber, channel: 'sms' });
     // .then(verification => console.log(verification))
@@ -13,7 +15,7 @@ class TwilioController {
   }
 
   async otpVerify(phnNumber, enteredOTP) {
-    return client.verify.services('VAaf49f2af1ff3f84cebeed1eb11a6b15f')
+    return client.verify.services(serviceId)
       .verificationChecks
       .create({ to: phnNumber, code: enteredOTP });
     // .then(verification => console.log(verification))
