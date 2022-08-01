08b2562ec5fed6d71092b458d65a1ae556f4b3ca
rkDas2949 <ranjeet.d@ignitiv.com>
2022-08-02 02:33:57 +0530

    payload revised , getting issue in normal registration now working fine

--- assets/src/domains/storefront/b2b_user_creation.js
+++ assets/src/domains/storefront/b2b_user_creation.js
@@ -5,57 +5,7 @@ const B2bAccountCreate = new B2BAccount();
 
 module.exports = function (context) {
   const payload = Object.assign({}, context.request.body.payload); // eslint-disable-line prefer-object-spread
-  // let p21AccountId = null;
-  // if (context.request.body.p21AccountId) {
-  //   p21AccountId = context.request.body.p21AccountId;
-  // }
-  // const client = new Client({
-  //   context: {
-  //     appKey: 'CosCon.coastal_registration.1.0.0.Release',
-  //     sharedSecret: '00c410dac56d49c7bc13ffc5d470ca44',
-  //   },
-  // });
-
   const clientContext = new Client();
-
-  // client.context['user-claims'] = null;
-
-  // const b2bAccount = new B2bAccountSDK(client);
-
-  // b2bAccount
-  //   .addAccount(
-  //     {},
-  //     {
-  //       body: payload,
-  //     }
-  //   )
-  //   .then(res => {
-  //     console.log(res);
-  //     const { id: accountId } = res;
-
-  //     return b2bAccount.addSalesRep({
-  //       accountId,
-  //       userId: 'e0f6008ea91544a59b0d667406e372c2',
-  //     });
-  //   })
-  //   .then(res => {
-  //     const { id: accountId } = res;
-  //     return b2bAccount.accountApprove({ accountId, status: 'approve' });
-  //   })
-  //   .then(res => {
-  //     if (p21AccountId) {
-  //       return b2bAccount.addB2BAccountAttribute(
-  //         { accountId: res.id, attributeFQN: 'tenant~account_id' },
-  //         {
-  //           body: {
-  //             fullyQualifiedName: 'tenant~account_id',
-  //             values: [p21AccountId],
-  //           },
-  //         }
-  //       );
-  //     }
-  //     return Promise.resolve('Creating New User');
-  //   })
   B2bAccountCreate.b2bAccountflow(clientContext, payload)
     .then(res => {
       console.log(res);
